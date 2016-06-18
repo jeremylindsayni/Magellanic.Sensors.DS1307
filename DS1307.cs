841b3561e75a6fa534b6e778a10ed94ea7b04291
jeremylindsayni <jeremy.lindsay@outlook.com>
2016-06-19 00:41:08 +0100

    Cleaned up with CodeMaid.

--- DS1307.cs
+++ DS1307.cs
@@ -7,16 +7,6 @@ namespace Magellanic.Sensors.DS1307
     {
         private byte I2C_ADDRESS = 0x68;
 
-        public override byte GetI2cAddress()
-        {
-            return I2C_ADDRESS;
-        }
-
-        public override byte[] GetDeviceId()
-        {
-            throw new NotImplementedException("This device does not have a unique device identifier.");
-        }
-
         public DateTime GetCurrentTime()
         {
             byte[] readBuffer = new byte[7];
@@ -26,9 +16,43 @@ namespace Magellanic.Sensors.DS1307
             return ConvertByteBufferToDateTime(readBuffer);
         }
 
+        public override byte[] GetDeviceId()
+        {
+            throw new NotImplementedException("This device does not have a unique device identifier.");
+        }
+
+        public override byte GetI2cAddress()
+        {
+            return I2C_ADDRESS;
+        }
+
         public void SetDateTime(DateTime dateTime)
         {
             this.Slave.Write(ConvertTimeToByteArray(dateTime));
+        }
+
+        private int BinaryCodedDecimalToInteger(int value)
+        {
+            var lowerNibble = value & 0x0F;
+            var upperNibble = value >> 4;
+
+            var multipleOfOne = lowerNibble;
+            var multipleOfTen = upperNibble * 10;
+
+            return multipleOfOne + multipleOfTen;
+        }
+
+        private DateTime ConvertByteBufferToDateTime(byte[] dateTimeBuffer)
+        {
+            var second = BinaryCodedDecimalToInteger(dateTimeBuffer[0]);
+            var minute = BinaryCodedDecimalToInteger(dateTimeBuffer[1]);
+            var hour = BinaryCodedDecimalToInteger(dateTimeBuffer[2]);
+            var dayofWeek = BinaryCodedDecimalToInteger(dateTimeBuffer[3]);
+            var day = BinaryCodedDecimalToInteger(dateTimeBuffer[4]);
+            var month = BinaryCodedDecimalToInteger(dateTimeBuffer[5]);
+            var year = 2000 + BinaryCodedDecimalToInteger(dateTimeBuffer[6]);
+
+            return new DateTime(year, month, day, hour, minute, second);
         }
 
         private byte[] ConvertTimeToByteArray(DateTime dateTime)
@@ -36,31 +60,18 @@ namespace Magellanic.Sensors.DS1307
             var dateTimeByteArray = new byte[8];
 
             dateTimeByteArray[0] = 0;
-            dateTimeByteArray[1] = DecimalToBinaryCodedDecimal(dateTime.Second);
-            dateTimeByteArray[2] = DecimalToBinaryCodedDecimal(dateTime.Minute);
-            dateTimeByteArray[3] = DecimalToBinaryCodedDecimal(dateTime.Hour);
-            dateTimeByteArray[4] = DecimalToBinaryCodedDecimal((byte)dateTime.DayOfWeek);
-            dateTimeByteArray[5] = DecimalToBinaryCodedDecimal(dateTime.Day);
-            dateTimeByteArray[6] = DecimalToBinaryCodedDecimal(dateTime.Month);
-            dateTimeByteArray[7] = DecimalToBinaryCodedDecimal(dateTime.Year - 2000);
-            
+            dateTimeByteArray[1] = IntegerToBinaryCodedDecimal(dateTime.Second);
+            dateTimeByteArray[2] = IntegerToBinaryCodedDecimal(dateTime.Minute);
+            dateTimeByteArray[3] = IntegerToBinaryCodedDecimal(dateTime.Hour);
+            dateTimeByteArray[4] = IntegerToBinaryCodedDecimal((byte)dateTime.DayOfWeek);
+            dateTimeByteArray[5] = IntegerToBinaryCodedDecimal(dateTime.Day);
+            dateTimeByteArray[6] = IntegerToBinaryCodedDecimal(dateTime.Month);
+            dateTimeByteArray[7] = IntegerToBinaryCodedDecimal(dateTime.Year - 2000);
+
             return dateTimeByteArray;
         }
 
-        private DateTime ConvertByteBufferToDateTime(byte[] dateTimeBuffer)
-        {
-            var second = BinaryCodedDecimalToDecimal(dateTimeBuffer[0]);
-            var minute = BinaryCodedDecimalToDecimal(dateTimeBuffer[1]);
-            var hour = BinaryCodedDecimalToDecimal(dateTimeBuffer[2]);
-            var dayofWeek = BinaryCodedDecimalToDecimal(dateTimeBuffer[3]);
-            var day = BinaryCodedDecimalToDecimal(dateTimeBuffer[4]);
-            var month = BinaryCodedDecimalToDecimal(dateTimeBuffer[5]);
-            var year = 2000 + BinaryCodedDecimalToDecimal(dateTimeBuffer[6]);
-
-            return new DateTime(year, month, day, hour, minute, second);
-        }
-
-        private byte DecimalToBinaryCodedDecimal(int value)
+        private byte IntegerToBinaryCodedDecimal(int value)
         {
             var multipleOfOne = value % 10;
             var multipleOfTen = value / 10;
@@ -70,17 +81,6 @@ namespace Magellanic.Sensors.DS1307
             var upperNibble = multipleOfTen << 4;
 
             return (byte)(lowerNibble + upperNibble);
-        }
-
-        private int BinaryCodedDecimalToDecimal(int value)
-        {
-            var lowerNibble = value;
-            var upperNibble = value >> 4;
-
-            var multipleOfOne = value & 0x0F;
-            var multipleOfTen = upperNibble * 10;
-
-            return multipleOfOne + multipleOfTen;
         }
     }
 }
